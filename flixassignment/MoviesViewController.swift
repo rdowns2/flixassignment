7617856f74eb2788b80cf2843e7efb2b1b537ce5
rdowns2 <rdowns2@toromail.csudh.edu>
2019-02-27 17:24:14 -0800

    Update MoviesViewController.swift

--- flixassignment/MoviesViewController.swift
+++ flixassignment/MoviesViewController.swift
@@ -57,6 +57,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let synopsis = movie["overview"] as! String
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
         return cell
     }
     override func didReceiveMemoryWarning() {
