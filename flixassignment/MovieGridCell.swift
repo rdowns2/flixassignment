ffad658a8dc24794f133dfe883854888eaa9a47d
macstudent <macstudent@dhillss-imac-13.local>
2019-02-28 18:23:23 -0800

    completed part 2 of the flex assignment

---
+++ flixassignment/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixassignment
+//
+//  Created by macstudent on 2/28/19.
+//  Copyright © 2019 rdowns2. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
